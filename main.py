6335d9d7fa94916444654b987fac4e632a70637c
shaikmalikbasha <shaikmalikbasha1998@gmail.com>
2022-04-15 16:01:34 +0530

    many-to-many completed

---
+++ main.py
@@ -0,0 +1,129 @@
+from typing import List
+
+from fastapi import Body, Depends, FastAPI
+from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session, relationship, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./local_database.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    try:
+        print("Trying to get the db cursor...")
+        db = SessionLocal()
+        yield db
+    except:
+        print("Database connection failed...")
+    finally:
+        print("Closing the connection...")
+        db.close()
+        print("Closed!")
+
+
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    is_active: bool
+    courses: list = []
+
+    class Config:
+        orm_mode = True
+
+
+class CourseResponse(BaseModel):
+    id: int
+    name: str
+    students: list = []
+
+    class Config:
+        orm_mode = True
+
+
+class StudentCourse(Base):
+    """
+    This is a Many to Many relatioship example
+    """
+
+    __tablename__ = "student_has_course"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    student_id = Column(Integer, ForeignKey("students.id"))
+    course_id = Column(Integer, ForeignKey("courses.id"))
+
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+    courses = relationship(
+        "Course", secondary=StudentCourse.__table__, back_populates="students"
+    )
+
+
+class Course(Base):
+    __tablename__ = "courses"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text, nullable=False)
+
+    students = relationship(
+        "Student", secondary=StudentCourse.__table__, back_populates="courses"
+    )
+
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+
+@app.get("/students", response_model=List[StudentResponse])
+def get_students(db: Session = Depends(get_db)):
+    return db.query(Student).all()
+
+
+@app.get("/courses", response_model=List[CourseResponse])
+def get_students(db: Session = Depends(get_db)):
+    return db.query(Course).all()
+
+
+@app.post("/students")
+def create_student(input_data: dict = Body(...), db: Session = Depends(get_db)):
+    print(input_data)
+    student = Student(name=input_data["name"])
+    db.add(student)
+    db.commit()
+    db.refresh(student)
+
+    return student
+
+
+@app.post("/courses")
+def create_course(input_data: dict = Body(...), db: Session = Depends(get_db)):
+    print(input_data)
+    course = Course(name=input_data["name"])
+    db.add(course)
+    db.commit()
+    db.refresh(course)
+
+    return course
+
+
+@app.post("/students/{student_id}/courses/{course_id}")
+def add_course_to_student(
+    student_id: int, course_id: int, db: Session = Depends(get_db)
+):
+    student_has_course = StudentCourse(student_id=student_id, course_id=course_id)
+    db.add(student_has_course)
+    db.commit()
+    db.refresh(student_has_course)
+
+    return student_has_course
